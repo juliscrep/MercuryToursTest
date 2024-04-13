d6928de9a14c806ed65883cd6ca797b719bddec1
juliscrep <julietascrepnik@gmail.com>
2024-04-13 15:03:20 -0300

    se agregan métodos a la clase Base

--- src/test/java/com/project/pom/Base.java
+++ src/test/java/com/project/pom/Base.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class Base {
     private WebDriver driver;
     public Base (WebDriver driver){
@@ -23,6 +26,39 @@ public class Base {
     //Hago un wrapping de los metodos de selenium webDriver
     public WebElement findElement(By locator){
         return  driver.findElement(locator);
+    }
+
+    public List<WebElement> findElements(By locator){ //metodo propio personalizado
+        return driver.findElements(locator); //metodo de selenium webDriver
+    }
+
+    public String getText(By locator){
+        return driver.findElement(locator).getText();
+    }
+
+    public String getText(WebElement element){  // sobrecarga/overload del metodo con distinto tipo de argumento de entrada
+        return element.getText();
+    }
+
+    public void type(String inputText, By locator){
+        driver.findElement(locator).sendKeys(inputText);
+    }
+
+    public void click(By locator){
+        driver.findElement(locator).click();
+    }
+
+    public boolean isDisplayed(By locator){
+        try {
+            return driver.findElement(locator).isDisplayed();
+        }catch (NoSuchElementException e){
+            return false; //retorna falso cuando el elemento no esta, no existe o fue modificado
+        }
+
+    }
+
+    public void visit(String url){
+        driver.get(url);
     }
 
 }
